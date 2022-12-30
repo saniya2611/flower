5f502d21f3f0dbc0a1c57aaf6c3c116e2e6dce96
saniya2611 <saniaatamboli26@gmail.com>
2022-12-30 07:31:41 +0530

    sbdka
    nceuinecel

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+const selectImage = document.getElementById("selectImage")
+
+const all = Array.from(document.querySelectorAll(".all"))
+
+const onImageColor = (eve) => {
+    let getImage = eve.target.value;
+    console.log(getImage);
+    all.forEach(div => div.classList.add('d-none'));
+
+    let getDivs = Array.from(document.querySelectorAll("."+getImage))
+    getDivs.forEach(div => div.classList.remove('d-none'))
+
+}
+ 
+
+
+
+
+selectImage.addEventListener("change", onImageColor)
+
+
+
+
+
+
+
+
+//selectColor.addEventListener("change", onChangeColor)
+
